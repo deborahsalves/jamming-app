2a7dbfd721b42076de7b5fd1e993b00fb4f97ded
deborahsalves <salves.deborah@gmail.com>
2022-02-21 12:10:38 -0300

    Steps 40-73
    Add/remove tracks from playlist, update playlist name, plus SearchBar functionality w/out API connection

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -30,10 +30,63 @@ class App extends React.Component {
             ],
             playlistName: 'hard-coded',
             playlistTracks: [
-                {name: 'track1'},
-                'track3'
+                {
+                    name: 'playlistTrack 1',
+                    artist: 'playlistArtist 1',
+                    album: 'playlistAlbum 1',
+                    id: '1'
+                },
+                {
+                    name: 'playlistTrack 2',
+                    artist: 'playlistArtist 2',
+                    album: 'playlistAlbum 2',
+                    id: '2'
+                },
+                {
+                    name: 'playlistTrack 3',
+                    artist: 'playlistArtist 3',
+                    album: 'playlistAlbum 3',
+                    id: '3'
+                },
             ]
         };
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+        this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlaylist = this.savePlaylist.bind(this);
+        this.search = this.search.bind(this);
+    }
+
+    search(searchTerm) {
+        console.log(`in App.js search(): ${searchTerm}`);
+    }
+
+    addTrack(track) {
+        const currentPlaylist = this.state.playlistTracks ;
+        const isInPlaylist = currentPlaylist.find(
+            trackInPlaylist => trackInPlaylist.id === track.id);
+        if (isInPlaylist) {
+            return;
+        } 
+        currentPlaylist.push(track);
+        this.setState({ playlistTracks: currentPlaylist });
+    }
+
+    removeTrack(track) {
+        let currentPlaylist = this.state.playlistTracks
+        currentPlaylist = currentPlaylist.filter(trackInPlaylist => 
+            trackInPlaylist.id !== track.id);
+
+        this.setState({ playlistTracks: currentPlaylist });
+    }
+
+    updatePlaylistName(name) {
+        this.setState({ playlistName: name});
+    }
+
+    savePlaylist() {
+        const trackURIs = this.state.playlistTracks.map(track => track.uri);
+        return trackURIs;
     }
 
     render() {
@@ -41,10 +94,16 @@ class App extends React.Component {
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    <SearchBar />
+                    <SearchBar onSearch={this.search}/>
                     <div className="App-playlist">
-                        <SearchResults searchResults={this.state.searchResults}/>
-                        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+                        <SearchResults searchResults={this.state.searchResults} 
+                                       onAdd={this.addTrack} 
+                                       />
+                        <Playlist playlistName={this.state.playlistName} 
+                                  onNameChange={this.updatePlaylistName}
+                                  playlistTracks={this.state.playlistTracks}
+                                  onRemove={this.removeTrack}
+                                  onSave={this.savePlaylist}/>
                     </div>
                 </div>
             </div>
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,31 @@ import React from "react";
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+        }
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    search() {
+        console.log(`in SearchBar.js search(): ${this.state.searchTerm}`);
+        this.props.onSearch(this.state.searchTerm);
+    }
+    handleTermChange(e) {
+        const searchInput = e.target.value;
+        console.log(`in SearchBar.js handle: ${searchInput}`);
+        this.setState({ searchTerm: searchInput});
+    }
     render() {
         return(
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input placeholder="Enter A Song, Album, or Artist" 
+                       onChange={this.handleTermChange}/>
+                <button className="SearchButton"
+                        onClick={this.search}>
+                            SEARCH</button>
                 </div>
         );
     }
